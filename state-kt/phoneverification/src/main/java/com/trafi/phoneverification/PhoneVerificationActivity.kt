0bf144db4c1f3f4652cdc6a7f8ff9b63a0ab0012
Justas Medeisis <justas@trafi.com>
2019-05-01 01:31:08 +0200

    Skeleton phone verification sample module.

---
+++ state-kt/phoneverification/src/main/java/com/trafi/phoneverification/PhoneVerificationActivity.kt
@@ -0,0 +1,13 @@
+package com.trafi.phoneverification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneVerificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.phone_verification_activity)
+    }
+
+}
