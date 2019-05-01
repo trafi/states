2dd9c9afd5b9070ecee2a0d74f25ec190e98805a
Justas Medeisis <justas@trafi.com>
2019-05-01 08:51:59 +0200

    Phone verification sample UI.

--- state-kt/phoneverification/src/main/java/com/trafi/phoneverification/PhoneVerificationActivity.kt
+++ state-kt/phoneverification/src/main/java/com/trafi/phoneverification/PhoneVerificationActivity.kt
@@ -8,6 +8,11 @@ class PhoneVerificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.phone_verification_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PhoneVerificationFragment.newInstance())
+                .commitNow()
+        }
     }
 
 }
